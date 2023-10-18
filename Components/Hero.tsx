8449588dfc394feca833c754fea8ee52d1e0c58d
AllGamers20 <husseinmoubarackmonster@gmail.com>
2023-10-18 08:41:08 +0300

    first try of slider

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,43 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import Link from 'next/link'
 import {FaFacebook,FaWhatsapp,FaInstagram} from 'react-icons/fa'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 const Hero = () => {
-  return (
-    <div className='bg-[url("/img/Hero.jpg")] h-screen bg-cover bg-no-repeat bg-center bg-fixed flex items-center relative'>
-        <div className='absolute inset-0 bg-black/60  '></div>
-        <div className='container mx-auto px-4 z-10'>
-            <div className='text-start text-white flex flex-col  gap-[20px] md:gap-[40px]'>
-                <div>
-                    <h1 className=' text-xl text-lime-500 md:text-3xl tracking-wider'>FIND YOUR WANDERLUST</h1>
-                </div>
-                <div>
-                    <h1 className=' text-2xl md:text-5xl text-main font-semibold tracking-wider'>WITH US</h1>
-                </div>
-                <div className='bg-main h-[2px]  w-[400px]'></div>
-                <div>
-                    <p className='text-xl text-gray-300 tracking-widest'>best way to fly safetly</p>
-                </div>
-                <div>
-                    <Button text={'Click For Offers'} link={'#Packages'}/>
-                </div>
-            </div>
+    const slides = [
+        {
+          url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+        },
+    
+        {
+          url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+        },
+      ];
+    
+      const [currentIndex, setCurrentIndex] = useState(0);
+    
+      const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+      };
+    
+      const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+      };
+    
+      const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+        setCurrentIndex(slideIndex);
+      };
+    
+      return (
+        <div className='max-w-[1640px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+          <div
+            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            className='w-full h-full  bg-center bg-cover duration-500'
+          ></div>
+          {/* Left Arrow */}
+          <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactLeft onClick={prevSlide} size={30} />
+          </div>
+          {/* Right Arrow */}
+          <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={nextSlide} size={30} />
+          </div>
+          <div className='flex top-4 justify-center py-2'>
+            {slides.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+                className='text-2xl cursor-pointer'
+              >
+                <RxDotFilled />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className='hidden absolute right-20 top-32 z-10 md:flex flex-col gap-5 items-center'>
-            <div className='bg-main w-[1.5px] h-[250px]'></div>
-            <Link href={'https://www.facebook.com/SpeedbirdLebanon'} target='_blank'>
-                <FaFacebook size={25} className='text-main hover:translate-x-1.5 duration-300 hover:text-blue-700'/>
-            </Link>
-            <Link href={'https://wa.me/+96171367510'} target='_blank'>
-                <FaWhatsapp size={25} className='text-main hover:translate-x-1.5 duration-300 hover:text-green-500'/>
-            </Link>
-            <Link href={'https://www.instagram.com/speedbirdtravellb/'} target='_blank'>
-                <FaInstagram size={25} className='text-main hover:translate-x-1.5 duration-300 hover:text-yellow-300'/>
-            </Link>
-        </div>
-    </div>
-  )
+      );
 }
 
 export default Hero
