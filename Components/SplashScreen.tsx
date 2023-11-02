2ca7a68bdc6d63786856fbfe4de6a60acee21ce3
All_Gamers20 <husseinmoubarackmonster@gmail.com>
2023-11-02 15:38:39 +0200

    make splash screen with animation

--- Components/SplashScreen.tsx
+++ Components/SplashScreen.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import React from 'react';
+import anime from 'animejs';
+import Image from 'next/image';
 
-const SplashScreen = () => {
+const SplashScreen = ({ finishLoading }: { finishLoading: any }) => {
+  const [isMount, setisMount] = useState(false);
+
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
+
+    // First animation: Scale up the logo
+    loader.add({
+      targets: "#Logo",
+      scale: 1,
+      duration: 400,
+      easing: "easeInOutExpo",
+    });
+    loader.add({
+      targets: "#Logo",
+      scale: 1.50,
+      duration: 300,
+      easing: "easeInOutExpo",
+    });
+
+    // Second animation: Move the logo to the right and fade out
+    loader.add({
+      targets: "#Logo",
+      translateX: '100%',
+      opacity: 0,
+      duration: 600,
+      easing: "easeOutExpo",
+      delay: 200, // Delay after the first animation
+    });
+  };
+
+  useEffect(() => {
+    // Add a delay before starting the fade-in animation
+    const fadeInTimeout = setTimeout(() => {
+      setisMount(true);
+    }, 600);
+
+    // Start the animations
+    animate();
+
+    return () => {
+      clearTimeout(fadeInTimeout);
+    };
+  }, []);
+
   return (
-    <div>SplashScreen</div>
-  )
-}
+    <div className={`flex h-screen items-center justify-center ${isMount ? 'fade-in' : ''} bg-parallex bg-fixed bg-cover bg-no-repeat`}>
+      <Image id='Logo' src="/img/Logo.png" alt='' width={200} height={200} />
+    </div>
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Navbar from '@/Components/navbar'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '@/Components/Hero'
 import Product from '@/Components/Product'
 import Head from 'next/head'
@@ -10,18 +11,35 @@ import SplashScreen from '@/Components/SplashScreen'
 import {usePathname} from 'next/navigation'
 const index = () => {
   const pathname = usePathname();
+  const isHome = pathname ==="/"
+  const [isLoading, setisLoading] = useState(isHome);
+  useEffect(() => {
+    if(isLoading) return
+
+    
+  },[isLoading])
+  
   return (
     <>
         <Head>
           <title>Speed Bird</title>
           <link rel="shortcut icon" href="Logo.ico" type="image/x-icon" />
         </Head>
-        <Navbar/>
-        <Hero/>
-        <Product/>
-        <Services/>
-        <AboutUs/>
-        <Footer/>
+          {isLoading && isHome ? (
+            <SplashScreen finishLoading={()=> setisLoading(false)}/>
+          ):(
+            <>
+            <Navbar/>
+            <Hero/>
+            <Product/>
+            <Services/>
+            <AboutUs/>
+            <Footer/>
+            </>
+          )
+          
+        }
+
     </>
 
   )
