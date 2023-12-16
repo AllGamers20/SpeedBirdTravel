bb589afba75ace12a034c4bae4592ba3da3b2cc1
AllGamers20 <husseinmoubarackmonster@gmail.com>
2023-12-16 06:29:57 +0200

    fix font

--- pages/_fonts.tsx
+++ pages/_fonts.tsx
@@ -3,21 +3,21 @@ import { Nunito } from 'next/font/google'
 import React from 'react'
 
 export const dale = LocalFont({
-    src: "../public/fonts/Dale-Adventure.woff2",
+    src: "../public/fonts/Dale-Adventure.ttf",
     weight: '400',
     style: 'normal',
     variable: '--font-dale',
 
 })
 export const CampFire = LocalFont({
-    src: "../public/fonts/Campfire.woff2",
+    src: "../public/fonts/Campfire.ttf",
     weight: '400',
     style: 'normal',
     variable: '--font-CampFire',
 
 })
 export const AirSpace = LocalFont({
-    src: "../public/fonts/AirSpace.woff2",
+    src: "../public/fonts/AirSpace.ttf",
     weight: '400',
     style: 'normal',
     variable: '--font-AirSpace',
